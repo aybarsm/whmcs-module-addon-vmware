542c36a2e20e0c3f1565013190ac38cab386bd01
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2014-02-26 19:04:55 +0000

    Implement the vmware_config function for addon settings

--- vmware.php
+++ vmware.php
@@ -1,3 +1,6 @@
 <?php
 function vmware_config() {
     $configarray = array(
+    "name" => "VMware",
+	"fields" => array(
+			"cronkey" => array ( "FriendlyName" => "Cron Key", "Type" => "password", "Size" => "50" )
