df69c0b347f2f4bc5b8882d4bcd5d7fa8cdae017
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2014-08-06 09:39:37 +0100

    Add fallback for when VM details cannot be fetched

--- vmware.php
+++ vmware.php
@@ -213,3 +213,5 @@ echo '<div id="tab_content">'.$hata.'
 <table cellspacing="2" cellpadding="3" border="0" width="100%" class="form">
 <tbody><tr>
 <td class="fieldarea">IP Adresi <input name="ip" type="Text" class="textbox" id="ip" value=""><input name="submitip" type="hidden" id="submitip" value="1"> Eklemek istediğiniz IP adresini yazın</td>
+<td class="fieldarea" style="width:150px;"><input name="Submit" type="submit" class="button" value="Ekle"></td></tr>
+</tbody></table>
