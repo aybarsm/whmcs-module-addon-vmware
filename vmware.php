a013944f448bf6fa71a490e57ad0f7f5237943cf
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2014-08-14 17:59:04 +0100

    Ensure all database fields have consistent collation

--- vmware.php
+++ vmware.php
@@ -223,3 +223,5 @@ if($_GET[addip]){
 } else {
 
 echo '<br/><form id="form1" name="formadd" method="post" action="addonmodules.php?module=vmware"><input name="updateextr" type="hidden" value="1" /><table class="datatable" width="100%" border="0" cellspacing="1" cellpadding="3">
+	<tr>
+		<th width="23%">IP address</th>
